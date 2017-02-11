a3eeeae4e33000b0f5a84448f778a333e7e6fe5f
jtparrett <jtparrett@me.com>
2017-02-11 18:37:27 +0000

    object fit

--- src/javascripts/application.js
+++ src/javascripts/application.js
@@ -1,3 +1,9 @@
+import $ from 'jquery'
 import carousel from './modules/carousel'
+import ObjectFit from './vendor/object-fit'
 
 carousel()
+
+$('.carousel__image').each(function(){
+  new ObjectFit(this)
+})
