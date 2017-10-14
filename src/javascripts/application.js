6cab53e943c3c234797b8be4d44640f096e32c5b
jtparrett <jtparrett@me.com>
2017-10-14 10:26:13 +0100

    react app setup

--- src/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery'
-import carousel from './modules/carousel'
-import ObjectFit from './vendor/object-fit'
-
-carousel()
-
-$('.carousel__image').each(function(){
-  new ObjectFit(this)
-})
